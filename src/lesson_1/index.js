57633cd4a57eae6b71193a303a891531126f4421
vaheshakhpazyan <vaheshakhpazyan92@gmail.com>
2020-07-09 17:03:51 +0400

    init lesson 1

---
+++ src/lesson_1/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css';
+
+/// lesson 1 state 
+function Lesson() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+      </header>
+    </div>
+  );
+}
+
+export default Lesson;
