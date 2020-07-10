863f29a8cabe3ed2fdf5f3b37c1b36c0d5917ca6
vaheshakhpazyan <vaheshakhpazyan92@gmail.com>
2020-07-10 11:50:16 +0400

    init lesson 1

---
+++ src/lesson_1/colorPicker.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ColorPicker extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
