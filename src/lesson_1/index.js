4f5f7d38bbb913afde43e753732d435829618a8e
vaheshakhpazyan <vahe-9231@mail.ru>
2020-07-10 11:51:25 +0400

    init lesson_1

--- src/lesson_1/index.js
+++ src/lesson_1/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import '../App.css';
-
-/// lesson 1 state 
+import React from "react";
+import "../App.css";
+import Clock from "./clock";
+import Counter from "./counter";
+import Tasks from "./task";
+/// lesson 1 state
 function Lesson() {
   return (
     <div className="App">
       <header className="App-header">
-       
+        <Clock />
+        <Counter />
+        <Tasks />
       </header>
     </div>
   );
